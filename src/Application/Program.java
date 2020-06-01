19fafac0c9f79c748941f35b83fe9a25ef5ebad7
Diogo de Araujo <diogodearaujo21@yahoo.com.br>
2020-06-01 15:36:04 -0300

    Seller entity class

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,13 +1,21 @@
 package Application;
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(1, "Diogo", "diogo@gmail.com", new Date(), 5000.0, obj);
+		System.out.println(seller);
 		
 	}
 
