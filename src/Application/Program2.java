64aa925396b43a032a1208435f26224191d1b3fd
Diogo de Araujo <diogodearaujo21@yahoo.com.br>
2020-06-08 14:41:23 -0300

    insert implementation Department

---
+++ src/Application/Program2.java
@@ -0,0 +1,20 @@
+package Application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("\n=== Test 1 INSERT ====");
+		Department department = new Department(null, "Music");
+		departmentDao.insert(department);
+		System.out.println("Novo Departamento incluso: " + department.getId());
+		
+	}
+
+}
